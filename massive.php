feb56b6470a0c53d7c3b36f4da6e78ab2c4a45de
Evgenii.Goncharov <hichi411@gmail.com>
2023-12-28 18:35:21 +0600

    test

---
+++ massive.php
@@ -0,0 +1,29 @@
+<?php
+
+//$unset = "unset(\$array[\$index]) = удаляет элемент массива по индексу \n";
+// count($massive) считает количество элементов;
+
+//$animals = ['unset' => "Чтобы удалить элемента массива в PHP, нужно использовать конструкцию unset",
+ //'dogs', 'test' => "enymany", null];
+$cities = ['moscow', 'london', 'berlin', 'porto'];
+
+function apply(array $arr, string $command, int $index, $newThink)
+{
+    $result = $arr;
+    switch ($command) {
+        case "reset":
+            $result = [];
+        break;
+        case "remove":
+            unset($result[$index]);
+        break;
+        case "change":
+            
+
+    }
+}
+ 
+
+
+
+
