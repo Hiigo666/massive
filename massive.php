fb664fee457672f01795f0d3bd863949d9ff2014
Evgenii.Goncharov <hichi411@gmail.com>
2023-12-29 15:54:04 +0600

    learn foreach

--- massive.php
+++ massive.php
@@ -5,9 +5,10 @@
 
 //$animals = ['unset' => "Чтобы удалить элемента массива в PHP, нужно использовать конструкцию unset",
  //'dogs', 'test' => "enymany", null];
-$cities = ['moscow', 'london', 'berlin', 'porto'];
+$cities = ['moscow', 'london', 'berlin'];
+$arr = [ 1, 2, 3, 4, $cities];
 
-function apply(array $arr, string $command, int $index, $newThink)
+function apply(array $arr, string $command, int $index = null, $newThink = null)
 {
     $result = $arr;
     switch ($command) {
@@ -18,10 +19,27 @@ function apply(array $arr, string $command, int $index, $newThink)
             unset($result[$index]);
         break;
         case "change":
+            $result[$index] = $newThink;
+        break;
     }
+    return $result;
 }
  
 
-
-
-
+//for( $i = 0; $i < count($arr); $i++) {
+//    var_dump($arr[$i]);
+  //  }
+$dada =[];
+foreach ($arr as $value){
+ //   var_dump($value);
+ if (!is_array($value)){
+ $dada[] = $value;
+ }
+    if (is_array($value))
+    {
+        foreach($value as $item) {
+            $dada[] = $item;
+        }
+    }
+}
+print_r($dada);
